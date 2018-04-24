72ac4d14bb5bef2ea5ba2cdf29f6441c12b61779
Yu-Jye <yu.jye.tung@gmail.com>
2018-04-23 21:46:22 -0400

    Test out asciinema

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -8,25 +8,31 @@ build(){
 }
 
 # Given path to a markdown file, open its corresponding source.txt
+# Use Case: I have just added a new picture to Python_Reversing.md in
+# contents/languages/Python_Reversing.md and I want to open its corresponding
+# sources.txt, which can be found in images/languages/Python_Reversing/sources.txt,
+# to document where I got that image from
 get_source(){
-    file=`echo $1 | awk -F'/' '{print $NF}'`
-    folder=`echo $1 | awk -F'/' '{print $(NF-1)}'`
+    relative_path=`sed "s|\.\/||" <<< $1`
+    absolute_path=`echo $(pwd)`/${relative_path}
+    file=`echo ${absolute_path} | awk -F'/' '{print $NF}'`
+    folder=`echo ${absolute_path} | awk -F'/' '{print $(NF-1)}'`
 
     # Checks if file (without the .md extension) is the same string as folder
     if [ "${file%.*}" == "$folder" ] 
     then 
-	file_location=`sed -e "s|contents|images|" -e "s|${file}|sources.txt|" <<< $1`
+	file_location=`sed -e "s|contents|images|" -e "s|${file}|sources.txt|" <<< ${absolute_path}`
     else
-	file_location=`sed -e "s|contents|images|" -e "s|.md|/sources.txt|" <<< $1`
+	file_location=`sed -e "s|contents|images|" -e "s|.md|/sources.txt|" <<< ${absolute_path}`
     fi
 
-    vim $file_location
+    vim ${file_location}
 }
 
 # Remove trailing whitespaces
 cleanup(){
     for file in `find -name "*.txt" -o -name "*.py" -o -name "*.md"`
     do
-        sed -i 's/\s*$//g' $file
+        sed -i 's/\s*$//g' ${file}
     done
 }
